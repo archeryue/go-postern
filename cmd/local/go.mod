f03f30f99b5dde97ff2ce63748d1b9366416bfac
archeryue <archeryue7@gmail.com>
2021-07-25 16:27:48 +0800

    ADD: local logic

--- cmd/local/go.mod
+++ cmd/local/go.mod
@@ -2,4 +2,6 @@ module github/archeryue/go-postern/cmd/local
 
 go 1.16
 
-require github.com/archeryue/go-postern/postern v0.0.0-20210725065308-ef373a9f34af
+require github.com/archeryue/go-postern/postern v0.0.0-20210725071044-0a38413c025f
+
+replace github.com/archeryue/go-postern/postern => ../../postern
--- cmd/local/local.go
+++ cmd/local/local.go
@@ -1,21 +1,44 @@
 package local
 
 import (
-	"fmt"
+	"log"
+	"net"
 	"os"
+	"strconv"
 
 	pst "github.com/archeryue/go-postern/postern"
 )
+
+func handle(config *pst.Config, conn net.Conn) {
+	defer conn.Close() 
+	//TODO: sock5 handshake, get local request
+	//TODO: send request to remote, get response
+	//TODO: send response back to local
+}
+
+func serve(config *pst.Config) {
+	listener, err := net.Listen("tcp", ":" + strconv.Itoa(config.LocalPort))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("start serving, port : %v\n", config.LocalPort)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("accept error: ", err)
+			continue
+		}
+		go handle(config, conn)
+	}
+}
 
 func main() {
 	path := os.Args[1]
 	config, err := pst.LoadConfig(path)
 	if err != nil {
-		fmt.Println("Config Error: " + path)
+		log.Printf("Config Error: %s\n", path)
 		return
 	}
-
-	cipher := pst.NewCipher(config.Key)
-
-	fmt.Println("vim-go")
+	serve(config)
 }
