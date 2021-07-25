527783633b8779fbc07a636f4b5e0ee5a17c9933
archeryue <archeryue7@gmail.com>
2021-07-25 16:46:56 +0800

    ADD: sock5 file to handle protocol

--- postern/darkconn.go
+++ postern/darkconn.go
@@ -2,8 +2,6 @@ package postern
 
 import (
 	"net"
-	"strconv"
-	"strings"
 )
 
 type DarkConn struct {
@@ -15,31 +13,13 @@ func NewConn(conn net.Conn, cipher *Cipher) *DarkConn {
 	return &DarkConn{conn, cipher}
 }
 
-func sock5head(dest string) []byte {
-	arr := strings.Split(dest, ":")
-	ip := arr[0]
-	port, _ := strconv.Atoi(arr[1])
-
-	ipLen := len(ip)
-	headLen := 1 + 1 + ipLen + 2 // type + ipLen + ipStr + port
-	buf := make([]byte, headLen, headLen)
-
-	buf[0] = 3 // type 3 : domain name
-	buf[1] = byte(ipLen)
-	copy(buf[2:], ip)
-	buf[2 + ipLen] = byte((port >> 8) & 0xFF) // high 8 bits
-	buf[2 + ipLen + 1] = byte(port & 0xFF) // low 8 bits
-
-	return buf
-}
-
 func DarkDial(dest, remote string, cipher *Cipher) (conn *DarkConn, err error) {
 	c, err := net.Dial("tcp", remote)
-	if (err != nil) {
+	if err != nil {
 		return
 	}
 	conn = NewConn(c, cipher)
-	if _, err := conn.Write(sock5head(dest)); err != nil {
+	if _, err := conn.Write(GenHead(dest)); err != nil {
 		conn.Close()
 		return nil, err
 	}
@@ -49,8 +29,8 @@ func DarkDial(dest, remote string, cipher *Cipher) (conn *DarkConn, err error) {
 func (conn DarkConn) Read(data []byte) (n int, err error) {
 	buf := make([]byte, len(data), len(data))
 	n, err = conn.Conn.Read(buf)
-	if (n > 0) {
-		conn.Decode(buf, data)	
+	if n > 0 {
+		conn.Decode(buf, data)
 	}
 	return
 }
