8af62174f7b04c982a258f17fef04895f7306e75
archeryue <archeryue7@gmail.com>
2021-07-25 15:27:54 +0800

    ADD: some local load config logic

---
+++ cmd/local/go.mod
@@ -0,0 +1,5 @@
+module github/archeryue/go-postern/cmd/local
+
+go 1.16
+
+require github.com/archeryue/go-postern/postern v0.0.0-20210725065308-ef373a9f34af
--- cmd/local/local.go
+++ cmd/local/local.go
@@ -1,9 +1,21 @@
-package main
+package local
 
 import (
 	"fmt"
+	"os"
+
+	pst "github.com/archeryue/go-postern/postern"
 )
 
 func main() {
+	path := os.Args[1]
+	config, err := pst.LoadConfig(path)
+	if err != nil {
+		fmt.Println("Config Error: " + path)
+		return
+	}
+
+	cipher := pst.NewCipher(config.Key)
+
 	fmt.Println("vim-go")
 }
