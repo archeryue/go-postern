2cdc42e78adbb0a6b92f740213e5bd33be846639
archeryue <archeryue7@gmail.com>
2021-07-25 14:22:14 +0800

    ADD: sock5 head in darkconn

--- postern/cipher_test.go
+++ postern/cipher_test.go
@@ -5,8 +5,10 @@ import "testing"
 func TestEncode(t *testing.T) {
 	cipher := NewCipher("foobar!")
 	raw := []byte("Hello World!")
-	data := cipher.Encode(raw)
-	msg := cipher.Decode(data)
+	data := make([]byte, len(raw), len(raw))
+	cipher.Encode(raw, data)
+	msg := make([]byte, len(data), len(data))
+	cipher.Decode(data, msg)
 	for i, v := range raw {
 		if v != msg[i] {
 			t.Fatalf("%s: encode error at index %d\n", msg, i)
--- postern/darkconn.go
+++ postern/darkconn.go
@@ -2,6 +2,8 @@ package postern
 
 import (
 	"net"
+	"strconv"
+	"strings"
 )
 
 type DarkConn struct {
@@ -13,13 +15,31 @@ func NewConn(conn net.Conn, cipher *Cipher) *DarkConn {
 	return &DarkConn{conn, cipher}
 }
 
-func DarkDial(addr string, cipher *Cipher) (conn *DarkConn, err error) {
-	c, err := net.Dial("tcp", addr)
+func sock5head(dest string) []byte {
+	arr := strings.Split(dest, ":")
+	ip := arr[0]
+	port, _ := strconv.Atoi(arr[1])
+	ipLen := len(ip)
+	headLen := 1 + 1 + ipLen + 2 // type + ipLen + ipStr + port
+	buf := make([]byte, headLen, headLen)
+	buf[0] = 3 // type 3 : domain name
+	buf[1] = byte(ipLen)
+	copy(buf[2:], ip)
+	buf[2 + ipLen] = byte((port >> 8) & 0xFF) // high 8 bits
+	buf[2 + ipLen + 1] = byte(port & 0xFF) // low 8 bits
+	return buf
+}
+
+func DarkDial(dest, remote string, cipher *Cipher) (conn *DarkConn, err error) {
+	c, err := net.Dial("tcp", remote)
 	if (err != nil) {
 		return
 	}
 	conn = NewConn(c, cipher)
-	// TODO: write protocol
+	if _, err := conn.Write(sock5head(dest)); err != nil {
+		conn.Close()
+		return nil, err
+	}
 	return
 }
 
