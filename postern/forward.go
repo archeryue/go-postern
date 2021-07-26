75cf8c124044c1fbe2e6365c727d39900452f82c
archeryue <archeryue7@gmail.com>
2021-07-26 16:25:14 +0800

    ADD: forward, finish postern

--- postern/forward.go
+++ postern/forward.go
@@ -1,8 +1,28 @@
 package postern
 
-import "net"
+import (
+	"io"
+	"log"
+	"net"
+)
 
 func Forward(in, out net.Conn, end chan byte) {
-	//TODO: read from in and write into out
+	buf := make([]byte, 4096)
+	for {
+		//TODO: timetou
+		n, err := in.Read(buf)
+		if n > 0 {
+			if _, err = out.Write(buf[:n]); err != nil {
+				log.Println("forward write error")
+				break
+			}
+		}
+		if err != nil {
+			if err != io.EOF {
+				log.Println("forward read error")
+			}
+			break
+		}	
+	}
 	end <- 1
 }
